85170398504c7f742af6973ce5017e3e0abc9855
vytranthanh2003 <vytranthanh2003!>
2024-10-02 15:40:57 +0700

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,19 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <v-app>
+    <Header /> <!-- Đảm bảo chỉ có một Header -->
+    <router-view />
+    <Footer /> <!-- Đảm bảo chỉ có một Footer -->
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Header from "./components/Header.vue";
+import Footer from "./components/Footer.vue";
 
 export default {
-  name: 'App',
   components: {
-    HelloWorld
+    Header,
+    Footer
   }
-}
+};
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,11 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from 'vue';
+import App from './App.vue';
+import vuetify from './plugins/vuetify';
+import router from './router';
+import './assets/styles.css';
 
-createApp(App).mount('#app')
+
+const app = createApp(App);
+app.use(vuetify);
+app.use(router); // Đảm bảo rằng router được sử dụng
+app.mount('#app');
